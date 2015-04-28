76af490171946f0d7c37ee39e49afff5dee92a06
Jon Ringvald <jvald8@gmail.com>
2015-04-28 15:37:39 -0700

    Creating module in assignment 2 directory

---
+++ assignment2/calc.js
@@ -0,0 +1,29 @@
+exports.add = function(a,b){
+  return a + b;
+};
+
+exports.subtract = function(a,b){
+  return a - b;
+};
+
+exports.multiply =  function(a,b){
+  return a * b;
+};
+
+exports.divide = function(a,b){
+  return a/b;
+};
+
+/*module.exports = function(a,b){
+  return {add:function(){
+    return a + b},
+  subtract:function(){
+    return a - b},
+  multiply:function() {
+    return a * b},
+  divide:function() {
+    return a / b}
+  }
+};*/
+
+
