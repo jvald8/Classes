6c598bf3f282a4f63fb904f64290427eb8d9be38
Jon Ringvald <jvald8@gmail.com>
2015-04-27 19:23:11 -0700

    Modifying calc.js to have 4 separate functions rather than return 1 function returning an object and adding all tests to 1 describe function

--- assignment1/test/calc_test.js
+++ assignment1/test/calc_test.js
@@ -5,29 +5,44 @@ var b = 3;
 
 describe('calc', function(){
   it('should add both parameters', function(){
-    expect(calc(a,b).add()).to.eql(a+b)
+    expect(calc.add(a,b)).to.eql(a+b)
+  });
+  it('should subtract the second parameter from the first', function(){
+    expect(calc.subtract(a,b)).to.eql(a-b)
+  });
+  it('should multiply parameters', function(){
+    expect(calc.multiply(a,b)).to.eql(a*b)
+  });
+  it('should divide the first parameter by the second', function(){
+    expect(calc.divide(a,b)).to.eql(a/b)
+  });
+})
+
+/*describe('calc', function(){
+  it('should add both parameters', function(){
+    expect(calc.add(a,b)).to.eql(a+b)
   });
   //console.log(a+b); //a little debugging
 });
 
 describe('calc', function(){
-  it('should add both parameters', function(){
+  it('should subtract the second parameter from the first', function(){
     expect(calc(a,b).subtract()).to.eql(a-b)
   });
   //console.log(a-b);
 });
 
 describe('calc', function(){
-  it('should add both parameters', function(){
+  it('should multiply parameters', function(){
     expect(calc(a,b).multiply()).to.eql(a*b)
   });
   //console.log(b);
 });
 
 describe('calc', function(){
-  it('should add both parameters', function(){
+  it('should divide the first parameter by the second', function(){
     expect(calc(a,b).divide()).to.eql(a/b)
   });
   //console.log(a); a little debugging
-});
+});*/
 
