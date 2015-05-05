999e3ff326f708a7ae393956978c42f5bc56bdcb
Jon Ringvald <jvald8@gmail.com>
2015-05-04 20:16:04 -0700

    Adding 'http://' to test url requests

--- assignment4/test/server_test.js
+++ assignment4/test/server_test.js
@@ -7,28 +7,28 @@ chai.use(chaiHttp);
 
 describe('server', function() {
   it('should return OK status on localhost homepage', function(done) {
-    chai.request('localhost:8888').get('/time').end(function(err, response)
+    chai.request('http://localhost:8888').get('/time').end(function(err, response)
      {
       expect(response).to.have.status(200);
       done();
      });
   });
   it('check to see if unexpected pathname gets 404 status code', function(done) {
-    chai.request('localhost:8888').get('/greett/jon').end(function(err, response)
+    chai.request('http://localhost:8888').get('/greett/jon').end(function(err, response)
      {
       expect(response).to.have.status(404);
       done();
      });
   });
   it('check to see if greet/name returns a string', function(done) {
-    chai.request('localhost:8888').get('/greet/jon').end(function(err, response)
+    chai.request('http://localhost:8888').get('/greet/jon').end(function(err, response)
      {
       expect(response.text).to.be.a('string');
       done();
      });
   });
   it('check to see if greet/name returns Hello name', function(done) {
-    chai.request('localhost:8888').get('/greet/jon').end(function(err, response)
+    chai.request('http://localhost:8888').get('/greet/jon').end(function(err, response)
      {
       expect(response.text).to.eql('Hello jon');
       done();
