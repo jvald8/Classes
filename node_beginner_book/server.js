254be6faa9af357bec48fea19ed16464536a5f19
Jon Ringvald <jvald8@gmail.com>
2015-05-04 18:32:00 -0700

    Updating the image upload app

--- node_beginner_book/server.js
+++ node_beginner_book/server.js
@@ -3,16 +3,12 @@ var url  = require('url');
 
 function start(route, handle) {
   function onRequest(request, response) {
+    var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " recieved.");
-    route(handle, pathname, response);
+    route(handle, pathname, response, request);
 
-    //response.writeHead(200, {"Content-Type": "text/plain"});
-    //var content = route(handle, pathname);
-    //response.write(content);
-    //response.end();
   }
-
 
   http.createServer(onRequest).listen(8888);
   console.log("Server has started");
